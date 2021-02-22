5bc881bb4f01119c0b26b22da829e2a73d0376e5
sajinmathew <sajinmathew@nousinfo.com>
2021-02-22 06:31:31 +0000

    API definition transferred by SwaggerHub

--- java/src/main/java/io/swagger/client/Pair.java
+++ java/src/main/java/io/swagger/client/Pair.java
@@ -12,7 +12,7 @@
 
 package io.swagger.client;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-02-22T06:30:28.653Z[GMT]")public class Pair {
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2021-02-22T06:31:31.166Z[GMT]")public class Pair {
     private String name = "";
     private String value = "";
 
